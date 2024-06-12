d9573adeaf8e9c125bc33e43608f3a3ea9c0bb1e
ardrasivadas <ardrac15@gmail.com>
2024-06-13 01:20:44 +0530

    created

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const mongoose= require("mongoose")
+const cors = require("cors")
+const { resultmodel } = require("./modelss/examresult")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://ardra:ardrac1543@cluster0.tehzzbx.mongodb.net/resultdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add", (req,res)=>{
+    let input = req.body
+    let result = new resultmodel(input)
+    result.save()
+    console.log(result)
+    res.json({status:"done"})
+})
+
+app.post("/search",(req,res)=>{
+    let input = req.body
+    resultmodel.find(input).then(
+(data)=> {
+    res.json(data)
+}
+    ).catch(
+(error)=>{
+    res.json(error)
+}
+    )
+})
+
+
+app.get("/view",(req,res)=>{
+    resultmodel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+})
+
+app.listen(8080,()=>{
+    console.log("server started")
+})
